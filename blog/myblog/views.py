60e3b09bdd4ebe4781802ec9c7f3a50b54301077
w1980224 <w1980224@westminster.ac.uk>
2025-04-22 22:19:37 +0100

    commit

--- blog/myblog/views.py
+++ blog/myblog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
-def test_view(request):
-    return HttpResponse("Test page")
+def index(request):
+    return render(request, "voting.html")
