99e7f650c1b5bc586e914a50414b9b95439b6473
w1980224 <w1980224@westminster.ac.uk>
2025-04-24 10:56:15 +0100

    commit

--- blog/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path(''. views.index, name='index'),
-    path('voting/', views.voting, name = 'voting'),
-]
--- blog/views.py
+++
@@ -1,4 +0,0 @@
-from django.shortcuts import render
-
-def voting(request):
-return render(request, 'blog/voting/html')
